9954590a9593a5d1ca459810a501543a27425785
Mohammad Tawfik <mohammad.tawfik@gmail.com>
2018-08-29 10:27:46 +0200

    Add files via upload

---
+++ ViscoMaxwell.m
@@ -0,0 +1,17 @@
+clear all
+close all
+
+Es=1;
+Cd=1;
+
+for ii=1:101
+  Omega=(ii-1)*.05;
+  Freq(ii)=Omega;
+  Estorage(ii)=Cd*Cd*Es*Omega*Omega/(Es*Es+Cd*Cd*Omega*Omega);
+  Eloss=Es*Es*Cd*Omega/(Es*Es+Cd*Cd*Omega*Omega);
+  Eta(ii)=Eloss/Estorage(ii);
+end
+
+plot(Freq,Eta,Freq,Estorage)
+axis([0,5,0,2])
+grid
